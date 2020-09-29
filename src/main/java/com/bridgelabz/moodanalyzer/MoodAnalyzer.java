c89edc7ecc19c6acfd83babe80a3344d95625901
Aditya Verma <aaadarths2357@gmail.com>
2020-09-29 12:43:17 +0530

    UC1

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -1,8 +1,13 @@
 package com.bridgelabz.moodanalyzer;
 
 public class MoodAnalyzer {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Mood Analyzer Program");
+
+	public String analyzeMood(String message) {
+		if(message.toLowerCase().contains("sad"))
+			return "Sad Mood";			
+		else
+			return "Happy Mood";
 	}
+	
 
 }
---
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.moodanalyzer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+	private MoodAnalyzer moodObject;
+	
+	@Before
+	public void initialize(){
+		moodObject=new MoodAnalyzer();
+	}
+	
+	@Test
+	public void moodTest() {
+		assertEquals("Happy Mood",moodObject.analyzeMood("I am in Happy Mood"));
+		assertEquals("Sad Mood",moodObject.analyzeMood("I am in Sad Mood"));
+	}	
+}
